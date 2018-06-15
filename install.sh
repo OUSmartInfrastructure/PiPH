372ea41ab52943e765558e52b0042f7a519ab3d1
Ohio University's Smart Infrastructure <33726869+OUSmartInfrastructure@users.noreply.github.com>
2018-06-15 13:40:13 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 apt install lftp
 chmod +x piph.sh
 echo "set net:reconnect-interval-base 0" >> /etc/lftp.conf
-echo "set ssl:verify-certificate-no" >> /etc/lftp.conf
+echo "set ssl:verify-certificate no" >> /etc/lftp.conf
 (crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/PiPH/piph.sh") | crontab -
