dccccf2933f9aaf454708dbeb768440aad14bab2
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2018-07-09 12:31:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt install lftp
 chmod +x piph.sh
+chmod 777 -R /usr/local/bin/PiPH
 echo "set net:reconnect-interval-base 0" >> /etc/lftp.conf
 echo "set ssl:verify-certificate no" >> /etc/lftp.conf
 (crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/PiPH/piph.sh") | crontab -
